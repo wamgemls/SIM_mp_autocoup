ea25fd7188a3347b958962eac4d0e58c107c15b8
wamgemls <wamgemls@gmail.com>
2023-01-21 18:46:07 +0100

    new_animation_func /Test2.py

--- Testing/Test2.py
+++ Testing/Test2.py
@@ -1,21 +1,17 @@
 import matplotlib.pyplot as plt
-import time
+import numpy as np
 
-def ShowGraph():
-    n = 2
-    j = 1
-    while j <= 10:
-        x = [i for i in range(n)]
-        y = [i for i in range(n)]
-        plt.plot(x, y, 'r-')
-        plt.ylim([0, 10])
-        plt.xlim([0, 10])
-        if j > 1:
-            plt.draw()
-        else:
-            plt.show(block=False)
-        time.sleep(1)
-        n += 1
-        j += 1
+x = np.linspace(0, 6*np.pi, 100)
+y = np.sin(x)
 
-ShowGraph()
+# You probably won't need this if you're embedding things in a tkinter plot...
+plt.ion()
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+line1, = ax.plot(x, y, 'r-') # Returns a tuple of line objects, thus the comma
+
+for phase in np.linspace(0, 10*np.pi, 500):
+    line1.set_ydata(np.sin(x + phase))
+    fig.canvas.draw()
+    fig.canvas.flush_events()
--- mp_autocoup/SIM_G2ClothoidPlanner.py
+++ mp_autocoup/SIM_G2ClothoidPlanner.py
@@ -56,30 +56,8 @@ class Simulation:
 
     def animation(self):
 
-        # First set up the figure, the axis, and the plot element we want to animate
-        fig = plt.figure()
-        ax = plt.axes(xlim=(0, 2), ylim=(-2, 2))
-        line, = ax.plot([], [], lw=2)
+        return
 
-        # initialization function: plot the background of each frame
-        def init():
-            line.set_data([], [])
-            return line,
-
-        # animation function.  This is called sequentially
-        def animate(i):
-            x = np.linspace(0, 2, 1000)
-            y = np.sin(2 * np.pi * (x - 0.01 * i))
-            line.set_data(x, y)
-            return line,
-
-        # call the animator.  blit=True means only re-draw the parts that have changed.
-        anim = animation.FuncAnimation(fig, animate, init_func=init,
-                                    frames=200, interval=20, blit=True)
-
-        plt.show(block)
-
-        
     def visualization(self, rnd = None):
 
         plt.figure(1)
