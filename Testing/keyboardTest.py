959e1738ca168c4e8467f506efd58d1acd55eebd
wamgemls <wamgemls@gmail.com>
2023-01-30 16:10:00 +0100

    keyboard pkg test

---
+++ Testing/keyboardTest.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib
+import keyboard
+
+
+while 1:
+
+    if keyboard.is_pressed('space'):
+        print("space pressed")
+    else:
+        print("not pressed")                          
+
+
+
