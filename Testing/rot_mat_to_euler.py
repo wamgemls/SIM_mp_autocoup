7cf14f265b986cf65b51de7d083b9b568b32ffb5
wamgemls <wamgemls@gmail.com>
2023-02-16 16:47:32 +0100

    update rotmat2euler

--- Testing/rot_mat_to_euler.py
+++ Testing/rot_mat_to_euler.py
@@ -1,8 +1,11 @@
 from scipy.spatial.transform import Rotation as R
 import numpy as np
 
-r = R.from_matrix([[-0.014338,0.999382,0.032588],
-			         [-0.573412,0.018413,-0.819035],
-			         [-0.819057,-0.030462,0.572867]])
+m = [	[-0.999870,-0.015680,0.010166,],
+		[0.015637,-0.999816,0.010879,],
+		[-0.003964,0.011037,0.999889]]
 
-print(r.as_euler('xyz',degrees=True))
+r = R.from_matrix(m)
+
+
+print(r.as_euler('xyz',degrees=False))
